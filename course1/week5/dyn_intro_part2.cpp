9ea07ae2da1e2be0bda5f94ed82742942ff57d16
kkagirins <kseniya.kagirina@zyfra.com>
2024-02-20 22:59:53 +0300

    ✨ feat(week5): Add part of tabulation algos

---
+++ course1/week5/dyn_intro_part2.cpp
@@ -0,0 +1,132 @@
+#include "timer.h"
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <iostream>
+#include <iomanip>
+
+// https://www.youtube.com/watch?v=oBt53YbR9Kk&t=1565s&ab_channel=freeCodeCamp.org
+
+
+std::ostream& operator<< (std::ostream &os, const std::vector<int> &data) {
+  size_t idx = 0;
+  for (const int &n : data) {
+    os << n << (idx < data.size() - 1 ? ":" : "");
+    idx++;
+  }
+  return os;
+}
+
+uint64_t fib(const int &n) {
+  std::vector<uint64_t> table(n + 1, 0);
+  table[1] = 1;
+  for (int i = 0; i <= n; i++) {
+    table[i + 1] += table[i];
+    if ((i + 2) <= n) {
+      table[i + 2] += table[i];
+    }
+  }
+  return table[n];
+}
+
+uint64_t GridTraveler(const int &rows, const int &cols) {
+  std::vector<std::vector<uint64_t>> table(rows + 1, std::vector<uint64_t>(cols + 1, 0));
+
+  table[1][1] = 1;
+  for (int i = 0; i <= rows; i++) {
+    for (int j = 0; j <= cols; j++) {
+      if((i + 1) <= rows) {
+        table[i + 1][j] += table[i][j];
+      }
+      if((j + 1) <= cols) {
+        table[i][j + 1] += table[i][j];
+      }
+    }
+  }
+  return table[rows][cols];
+}
+
+bool CanSun(const int &target, const std::vector<int> &sequence) {
+  std::vector<bool> table(target + 1, false);
+  // cause you always could generate zero number
+  table[0] = true;
+  for (int i = 0; i <= target; i++) {
+    if (table[i] == true) {
+      for (auto n : sequence) {
+        if ((i + n) <= target) {
+          table[i + n] = true;
+        }
+      }
+    }
+  }
+
+  return table[target];
+}
+
+struct HowSumResult {
+  bool canSum = false;
+  std::vector<int> howSum;
+   
+  HowSumResult() {
+    ;
+  }
+
+  HowSumResult(const bool &res, const std::vector<int> &seq) {
+    this->canSum = res;
+    this->howSum = seq;
+  }
+};
+
+std::ostream& operator<< (std::ostream &os,  HowSumResult &data) {
+  os << std::boolalpha << "can sum " << data.canSum << ", how sum " << data.howSum;
+  return os;
+}
+
+HowSumResult HowSum(const int &target, const std::vector<int> &sequence) {
+  std::vector<HowSumResult> table(target + 1, HowSumResult());
+  table[0] = HowSumResult(true, {});
+
+  for (int i = 0; i <= target; i++) {
+    // std::cout << "i " << i << std::endl;
+    // for (auto t : table) {
+    //   std::cout << t << std::endl;
+    // }
+
+    if (table[i].canSum == true) {
+      for (auto n : sequence) {
+        if ((i + n) <= target) {
+          table[i + n] = table[i];
+          table[i + n].howSum.push_back(n);
+          // std::cout << "i " << i << " n " << n << " t " << table[i + n] << std::endl;
+        }
+      }
+    }
+  }
+  return table[target];
+}
+
+int main () {
+  //fib
+  std::cout << "fib " << fib(6) <<  ", expected " << 8 << std::endl;
+  std::cout << "fib " << fib(7) <<  ", expected " << 13 << std::endl;
+  std::cout << "fib " << fib(8) <<  ", expected " << 21 << std::endl;
+  // std::cout << "fib " << fib(50) <<  ", expected " << 12586269025 << std::endl;
+
+  std::cout << "GridTraveler " << GridTraveler(1, 1) <<  ", expected " << 1 << std::endl;
+  std::cout << "GridTraveler " << GridTraveler(2, 3) <<  ", expected " << 3 << std::endl;
+  std::cout << "GridTraveler " << GridTraveler(3, 2) <<  ", expected " << 3 << std::endl;
+  std::cout << "GridTraveler " << GridTraveler(3, 3) <<  ", expected " << 6 << std::endl;
+  std::cout << "GridTraveler " << GridTraveler(18, 18) <<  ", expected " << 2333606220 << std::endl;
+
+  std::cout << std::boolalpha << "CanSun " << CanSun(7, {2, 3}) <<  ", expected " << true << std::endl;
+  std::cout << std::boolalpha << "CanSun " << CanSun(7, {5, 3, 4, 7}) <<  ", expected " << true << std::endl;
+  std::cout << std::boolalpha << "CanSun " << CanSun(7, {2, 4}) <<  ", expected " << false << std::endl;
+  std::cout << std::boolalpha << "CanSun " << CanSun(8, {2, 3, 5}) <<  ", expected " << true << std::endl;
+  std::cout << std::boolalpha << "CanSun " << CanSun(300, {7, 14}) <<  ", expected " << false << std::endl;
+
+  std::cout << "HowSum " << HowSum(7, {2, 3}).howSum <<  ", expected " << std::vector<int>({3, 2, 2}) << std::endl;
+  std::cout << "HowSum " << HowSum(7, {5, 3, 4, 7}).howSum <<  ", expected " << std::vector<int>({4, 3}) << std::endl;
+  std::cout << "HowSum " << HowSum(7, {2, 4}).howSum <<  ", expected " << std::vector<int>() << std::endl;
+  std::cout << "HowSum " << HowSum(8, {2, 3, 5}).howSum <<  ", expected " << std::vector<int>({2, 2, 2, 2}) << std::endl;
+  std::cout << "HowSum " << HowSum(300, {7, 14}).howSum <<  ", expected " << std::vector<int>() << std::endl;
+}
